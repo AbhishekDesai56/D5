fb1a79f3648511826eb60c9e5eb496213b9a8321
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-07-10 09:13:23 +0530

    [Abhishek] Add Calculate Wages for a month

--- EmoloyeeWage.java
+++ EmoloyeeWage.java
@@ -4,7 +4,9 @@ public class EmoloyeeWage {
 	public final static int isFullTime = 1;
 	public final static int isPartTime = 2;
 	public final static int EMP_RATE_PER_HOUR = 20; 
+	public final static int NUMBER_OF_WORKING_DAYS = 20;
     static int empHrs;
+    static int totalWorkingDays = 0, totalEmpWage = 0;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int isPresent = (int) (Math.floor(Math.random() * 10) % 2);
@@ -13,21 +15,27 @@ public class EmoloyeeWage {
 		} else {
 			System.out.println("Employee is present");
 		}
-			
-		if(isPresent == 1)
-		{
-			int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
-			switch (empCheck) {
-			case isFullTime:
-				empHrs = 8;
-				break;
-			case isPartTime:
-				empHrs = 4;
-			default:
-				break;
+		
+		while(totalWorkingDays < NUMBER_OF_WORKING_DAYS) {
+			int empWage = 0;
+			totalWorkingDays++;
+			if(isPresent == 1)
+			{
+				int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+				switch (empCheck) {
+				case isFullTime:
+					empHrs = 8;
+					break;
+				case isPartTime:
+					empHrs = 4;
+				default:
+					break;
+				}
 			}
+		    empWage = empHrs * EMP_RATE_PER_HOUR;
+			totalEmpWage += empWage;
+			
 		}
-		int empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Employee Wage: " + empWage);
+		System.out.println("Total Employee Wage: " + totalEmpWage);
 	}
 }
