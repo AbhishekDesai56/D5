8cad709a8295b3a16a65d97c879a121ab4368665
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-07-10 06:45:46 +0530

    Check Whether Employee is present or not

--- EmoloyeeWage.java
+++ EmoloyeeWage.java
@@ -4,7 +4,12 @@ public class EmoloyeeWage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int isPresent = (int) (Math.floor(Math.random() * 10) % 2);
+		if (isPresent == 0)
+			System.out.println("Employee is absent");
+		else
+			System.out.println("Employee is present");
+		
 	}
 
 }
