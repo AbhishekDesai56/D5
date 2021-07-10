47200352703be78cfd6dd4f2da1c0345e8b6187b
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-07-10 18:26:13 +0530

    [Abhishek] Add equality to check two line

--- LineComparison.java
+++ LineComparison.java
@@ -1,12 +1,26 @@
 package D5;
 
+
+ class CheckEqualityofTwoLine {	
+	public String LengthofLine(int x1, int y1, int x2, int y2) {
+		double lengthOfTheLine = (Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+		lengthOfTheLine=Math.sqrt(lengthOfTheLine); 
+		lengthOfTheLine = Math.floor(lengthOfTheLine);
+		return Double.toString(lengthOfTheLine);
+	}
+ }
+	
+	
 public class LineComparison {
 
 	public static void main(String[] args) {
-		int x1=3, y1=4, x2=4, y2=3;
-		double lengthOfTheLine = (Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
-		lengthOfTheLine=Math.sqrt(lengthOfTheLine); 
-		System.out.println("Length of the Line:" +lengthOfTheLine);
+		CheckEqualityofTwoLine cqotl = new CheckEqualityofTwoLine();
+		String lineOne = cqotl.LengthofLine(4, 3, 4, 4);
+		String lineTwo = cqotl.LengthofLine(4, 3, 4, 4);
+		
+		if(lineOne.equals(lineTwo)) 
+			System.out.println(lineOne + " is equal to " + lineTwo);
+		else
+			System.out.println(lineOne + " is not equal to " + lineTwo);
 	}
-
 }
