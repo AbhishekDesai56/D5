f21cc5a2d5aa78746ef26b891e3e2aff4884c762
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-07-10 11:09:53 +0530

    [Abhishek] Refactor code to class object to compute employee wage

--- EmoloyeeWage.java
+++ EmoloyeeWage.java
@@ -1,45 +1,61 @@
 package D5;
 
-public class EmoloyeeWage {
+class Employee
+{
 	public final static int isFullTime = 1;
 	public final static int isPartTime = 2;
 	public final static int EMP_RATE_PER_HOUR = 20; 
 	public final static int NUMBER_OF_WORKING_DAYS = 20;
 	public final static int MAX_HRS_IN_MONTH = 100;
+	public final static int PART_TIME_HOURS = 4;
+	public final static int FULL_TIME_HOURS = 8;
     static int empHrs;
     static int totalWorkingDays = 0, totalEmpWage = 0, totalEmpHrs = 0;
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int isPresent = (int) (Math.floor(Math.random() * 10) % 2);
-		if (isPresent == 0) {
+    
+    public void empWageCalc() {
+    	int empPresent = (int) (Math.floor(Math.random() * 10) % 2);
+    	boolean isPresent = (empPresent == 1) ? true : false;
+		if (isPresent) {
 			System.out.println("Employee is absent");
+			return;
 		} else {
 			System.out.println("Employee is present");
 		}
 		
 		while(totalEmpHrs <= MAX_HRS_IN_MONTH &&
 				totalWorkingDays < NUMBER_OF_WORKING_DAYS) {
-			int empWage = 0;
-			totalWorkingDays++;
-			if(isPresent == 1)
-			{
-				int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
-				switch (empCheck) {
-				case isFullTime:
-					empHrs = 8;
-					break;
-				case isPartTime:
-					empHrs = 4;
-				default:
-					empHrs = 0;
-				}
-			}
-			totalEmpHrs += empHrs;
-		    empWage = empHrs * EMP_RATE_PER_HOUR;
-			totalEmpWage += empWage;
-			
+				totalWorkingDays++;
+				int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+				int empHrs = getWorkingHours(empCheck);
+				totalEmpHrs += empHrs;
 		}
+	
+		totalEmpWage = calcDailyWage(totalEmpHrs);
 		System.out.println("Total Employee Hours: " + totalEmpHrs);
 		System.out.println("Total Employee Wage: " + totalEmpWage);
+    }
+    
+	public int calcDailyWage(int empHrs) {
+		return empHrs * EMP_RATE_PER_HOUR;
+	}
+	
+	public  int getWorkingHours(int empCheck) {
+		switch (empCheck) {
+			case isPartTime:
+				return PART_TIME_HOURS;
+			case isFullTime:
+				return FULL_TIME_HOURS;
+			default:
+				return 0;
+		}
 	}
 }
+
+public class EmoloyeeWage {
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		e.empWageCalc();
+	}
+}
+
+
